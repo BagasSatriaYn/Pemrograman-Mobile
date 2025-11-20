15d3aa81a10d9a2fdb3b3ddb3aee3c72b7dd394f
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-11-20 21:24:59 +0700

    Praktikum 2 Menangani JSON tidak kompatibel

--- week_12/lib/main.dart
+++ week_12/lib/main.dart
@@ -1,25 +1,57 @@
-import 'User.dart';
+import 'package:flutter/material.dart';
+import 'package:week_12/User.dart';
 
 void main() {
-  // Object Dart ke JSON
+  print(' === DEBUG: Check JSON s=Structure ===');
+
+
   User user = User(
-    id: 1,
-    name: 'John Doe',
-    email: 'john@example.com',
-    createdAt: DateTime.now(),
-  );
+    id: 1, 
+    name: 'John Doe', 
+    email: 'john@example.com', 
+    createdAt: DateTime.now());
 
-  Map<String, dynamic> userJson = user.toJson();
-  print('User JSON: $userJson');
+    Map<String, dynamic> userJson = user.toJson();
+    print('User.toJson() result: $userJson');
+    print('Field names: ${userJson.keys.toList()}');
 
-  // JSON ke Object Dart
-  Map<String, dynamic> jsonData = {
-    'id': 2,
-    'name': 'Jane Doe',
-    'email': 'jane@example.com',
-    'created_at': '2024-01-01T10:00:00.000Z',
-  };
+    print('\n === TEST: JSON to Object ===');
+    //gunakan nama field names yang sama dengan toJson() result
+    Map<String, dynamic> jsonData ={
+      'id': 2,
+      'name': 'Jane Doe',
+      'email': 'jane@example.com',
+      'created_at': '2024-01-01T10:00:00.000Z',
+    };
+    // Debug: print Json structure
+    print('JSON data to parse: $jsonData');
+    print('JSON keys: ${jsonData.keys.toList()}');
+    print('id: ${jsonData['id']} (type: ${jsonData['id'].runtimeType})');
+    print('name: ${jsonData['name']} (type: ${jsonData['name'].runtimeType})');
+    print('email: ${jsonData['email']} (type: ${jsonData['email'].runtimeType})');
+    print('createdAt: ${jsonData['createdAt']} (type: ${jsonData['createdAt'].runtimeType})',);
 
-  User userFromJson = User.fromJson(jsonData);
-  print('User from JSON: ${userFromJson.name}');
+    try{
+      User userFromJson = User.fromJson(jsonData);
+      print('Success: User from JSON: $userFromJson');
+    } catch (e, stack) {
+      print('ERROR: $e');
+      print('Stack trace: $stack');
+    }
+
+    print('\n === TEST: Handle Missing Fields ===');
+
+    Map<String, dynamic> incompleteJson ={
+      'id': 3,
+      //'name': missing;
+      'email': 'test@example.com',
+      //'createdAt': missing
+    };
+
+    try{
+      User userFromIncomplete = User.fromJson(incompleteJson);
+      print('User from incomplete JSON: $userFromIncomplete');
+     } catch (e) {
+      print('Error with iincomplete JSON: $e');
+     }
 }
