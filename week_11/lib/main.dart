b82796108003419a7890ae9853e9c3d746e53db7
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-11-20 20:34:55 +0700

    First Commit & View

---
+++ week_11/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:week_11/view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class AppSettings {
+  bool isDarkMode = false;
+  String userName = 'Teman';
+  int fontSize = 20;
+  
+
+  Color get backgroundColor {
+    return isDarkMode ? Colors.grey[900]! : Colors.white;
+  }
+
+  Color get textColor {
+    return isDarkMode ? Colors.white : Colors.black;
+  }
+
+  String get greeting {
+    return "Halo, $userName!";
+  }
+}
